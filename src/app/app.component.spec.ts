510e1a32befba6f29117218b1d29e99096501c52
Daniel Locatelli <daniel.nlocatelli@gmail.com>
2026-01-09 15:18:57 +0100

    debugging page not loading with vs code agent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'quickeys' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect((app as any).title).toEqual('quickeys');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withNavigationErrorHandler } from '@angular/router';
+import { provideZoneChangeDetection } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
       withNavigationErrorHandler((error) => {
