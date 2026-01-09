b6e252386a1b4ec26e8a6d6b539256b5515efab9
Daniel Locatelli <daniel.nlocatelli@gmail.com>
2026-01-09 14:37:26 +0100

    fix to renaming the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'dev-shortcuts' title`, () => {
+  it(`should have the 'quickeys' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dev-shortcuts');
+    expect((app as any).title).toEqual('quickeys');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dev-shortcuts');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, quickeys!');
   });
 });
